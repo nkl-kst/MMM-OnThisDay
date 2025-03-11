1fcc617690c3c138fd70bb0345a3f7a436553255
nkl-kst <24235974+nkl-kst@users.noreply.github.com>
2025-03-11 07:35:32 +0100

    Update progress bar with smooth loading indicator

--- src/ProgressUpdater.js
+++ src/ProgressUpdater.js
@@ -1,23 +1,30 @@
 /**
- * Updates the progress bar every 1s.
+ * Updates the progress bar immediately when it is created.
  */
 
-function updateProgress() {
+function updateProgress(mutations) {
+    // Find mutations in this module
+    const moduleMutations = [];
+    mutations.forEach(function (mutation) {
+        if (mutation.target.parentNode.classList.contains('MMM-OnThisDay')) {
+            moduleMutations.push(mutation);
+        }
+    });
+
+    // Do nothing if mutations are not in this module
+    if (moduleMutations.length <= 0) {
+        return;
+    }
+
+    // Find progress bar
     const progress = document.getElementById('mmm-otd-carousel-progress');
     if (!progress) {
         return;
     }
 
-    if (progress.value >= progress.max) {
-        return;
-    }
-
-    ++progress.value;
+    // Update progress
+    progress.value = 1;
 }
 
-(function scheduleUpdate() {
-    setTimeout(() => {
-        updateProgress();
-        scheduleUpdate();
-    }, 1000);
-})();
+const observer = new MutationObserver(updateProgress);
+observer.observe(document, { subtree: true, childList: true });
